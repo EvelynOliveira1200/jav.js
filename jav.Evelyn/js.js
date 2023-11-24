adecbe60c921c631e556a7806b8b218bc76368b8
Evelyn <145203060+EvelynOliveira1200@users.noreply.github.com>
2023-11-23 21:51:59 -0300

    Add files via upload

---
+++ jav.Evelyn/js.js
@@ -0,0 +1,104 @@
+//let num1 = 20;
+//let num2 = 50;
+
+//soma = num1 + num2;
+
+//console.log(soma);
+//soma = num1 + num2;
+//mult= num1 * num2;
+//sub= num1 - num2;
+//div = num1 / num2;
+//rest = num1 % num2;
+
+//console.log(soma);
+//console.log(mult);
+//console.log(sub);
+//console.log(div);
+//console.log(rest);
+
+//let num1 = 50;
+//let num2 = 80;
+//let num3 = 90;
+//let num4 = 30;
+
+//media = (num1 + num2 + num3 +num4) / 4;
+
+//console.log(media);
+
+//let num1 = 61
+//let num2 = 1.64
+
+//imc = num1 / (num2 * num2)
+
+//console.log(imc)
+
+//let nome = "Evelyn";
+//let sobrenome = "Oliveira"
+
+//console.log(`meu nome é ${nome} ${sobrenome}`);
+
+//let idade = 18
+
+//if (idade <=10) {
+    //console.log("A pessoa é uma criança");
+//}else if(idade > 10 && idade <= 17){
+    //console.log("A pessoa é um adolescente")
+//}else if(idade >= 18 && idade <= 59){
+    //console.log("A pessoa é um adulto")
+//}
+
+//let peso = 61;
+//let altura = 1.64;
+//imc = (peso / (altura * altura)).toFixed(2)
+
+//if(imc < 18.5){
+  //  console.log("Abaixo do peso")
+//}else if(imc >= 18.5 && imc <= 25){
+   // console.log("Peso normal")
+//}else if(imc >= 25 && imc <= 30){
+   // console.log("Acima do peso")
+//}else if(imc >= 30 && imc <= 40){
+ //   console.log("Obeso")
+//}else{
+//    console.log("Obesidade grave")
+//}
+
+//function soma (x, y){
+  //  return x + y;
+//}
+//console.log(soma(2, 3));
+//console.log(soma(10, 25));
+//console.log(soma(50, 100));
+
+//function imc(peso, altura){
+//return (peso / (altura * altura)).toFixed(2);
+//}
+//console.log(imc(61, 1.64));
+//console.log(imc(61, 1.64));
+//console.log(imc(61, 1.64));
+//console.log(imc(61, 1.64));
+//console.log(imc(61, 1.64));
+//false
+
+//function saudacao(nome){
+//    return`Boa noite ${nome}`
+//}
+//console.log(saudacao("Maria"));
+
+//const raiz = function (n){
+  //  return n ** 0.5;
+//}
+//console.log(raiz(9));
+
+//const raiz1 = (n) =>{
+ //   return n ** 0.5;
+//}
+//console.log(raiz1(81));
+
+//const raiz2 = n => n ** 0.5;
+//console.log(raiz2(6561));
+
+for (let i= 0; i<=10; i++){
+    console.log(`O número 7 x ${i} = ${7*i}`);
+
+}
